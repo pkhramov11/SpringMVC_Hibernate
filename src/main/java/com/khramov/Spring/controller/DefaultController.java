6a7eb5464be74ab3a46fa39f100b23c026b1b2dd
Pavel <pkhramov1@gmail.com>
2022-11-26 19:17:34 +0300

    update

--- src/main/java/com/khramov/Spring/controller/DefaultController.java
+++ src/main/java/com/khramov/Spring/controller/DefaultController.java
@@ -46,8 +46,8 @@ public class DefaultController {
 
     @RequestMapping("/deleteEmployee")
     public String deleteEmployee(@RequestParam("empId") int id) {
-        Employee employee1 = employeeService.getEmployee(id);
-        employeeService.deleteEmployee(employee1);
+        Employee employee = employeeService.getEmployee(id);
+        employeeService.deleteEmployee(employee);
         return "redirect:/";
     }
 
